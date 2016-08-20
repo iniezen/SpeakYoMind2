265a8622b2c2bb4928629618023d1a801e5f345d
Israel Niezen <israelniezen@Israels-MacBook-Pro.local>
2016-08-20 16:04:24 -0700

    bla

--- logic.js
+++ logic.js
@@ -1,3 +1,63 @@
+ $(".text_process_button").click(function(){
+    var textString = $("#searchInput").val().trim();
+    console.log(textString);
+
+        $.ajax({
+            url: 'https://twinword-sentiment-analysis.p.mashape.com/analyze/?text=',
+
+            type: 'GET', // The HTTP Method
+            data: {text: textString},
+            datatype: 'json',
+            success: function (result) {
+              
+        console.log(result);
+        console.log(result.type);
+        console.log(result.score);
+        $("#sentimentScorePanel").html(result.type+"= "+result.score);
+        $("#sentimentScore").html(result.score);
+        var barWidth= Math.abs(result.score*100)+"%";
+        console.log(parseInt(barWidth));
+        $('#bar').width(barWidth);
+        console.log((Math.abs(result.score*100)));
+        if(result.type==="negative"){
+          $('#bar').addClass("progress-bar-danger");
+        } 
+        
+          else if(result.type==="positive"){
+            $('#bar').addClass("progress-bar-success");
+          }
+
+            else {
+              $('#bar').addClass("progress-bar-warning");
+            };
+
+        console.log(result.keywords["0"].word);
+        console.log(result.keywords["0"].score);
+        console.log(result.keywords["1"].word);
+        console.log(result.keywords["1"].score);
+        console.log(result.keywords["2"].word);
+        console.log(result.keywords["2"].score);
+
+        $("#word1").html(result.keywords["0"].word+"= "+result.keywords["0"].score);
+        $("#word2").html(result.keywords["1"].word+"= "+result.keywords["1"].score);
+        $("#word3").html(result.keywords["2"].word+"= "+result.keywords["2"].score);  
+        $("#word4").html(result.keywords["3"].word+"= "+result.keywords["3"].score);
+        $("#word5").html(result.keywords["4"].word+"= "+result.keywords["4"].score);  
+            },
+            error: function (err) {
+                alert(err);
+            },
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader("X-Mashape-Authorization", "UZcvZdpyc2mshN5Bh0clQLUDQ5PFp1IMTstjsn24rmcVxEWTqG"); // Enter here your Mashape key
+            }
+        });
+
+    });
+
+
+
+
+
 var langs =
 [['Afrikaans',       ['af-ZA']],
  ['Bahasa Indonesia',['id-ID']],
@@ -242,60 +302,6 @@ function showButtons(style) {
 
 
 
- $(".text_process_button").click(function(){
-		var textString = $("#searchInput").val().trim();
-		console.log(textString);
 
-        $.ajax({
-            url: 'https://twinword-sentiment-analysis.p.mashape.com/analyze/?text=',
-
-            type: 'GET', // The HTTP Method
-            data: {text: textString},
-            datatype: 'json',
-            success: function (result) {
-            	
-				console.log(result);
-				console.log(result.type);
-				console.log(result.score);
-				$("#sentimentScorePanel").html(result.type+"= "+result.score);
-				$("#sentimentScore").html(result.score);
-				var barWidth= Math.abs(result.score*100)+"%";
-				console.log(parseInt(barWidth));
-				$('#bar').width(barWidth);
-				console.log((Math.abs(result.score*100)));
-				if(result.type==="negative"){
-					$('#bar').addClass("progress-bar-danger");
-				}	
-				
-					else if(result.type==="positive"){
-						$('#bar').addClass("progress-bar-success");
-					}
-
-						else {
-							$('#bar').addClass("progress-bar-warning");
-						};
-
-				console.log(result.keywords["0"].word);
-				console.log(result.keywords["0"].score);
-				console.log(result.keywords["1"].word);
-				console.log(result.keywords["1"].score);
-				console.log(result.keywords["2"].word);
-				console.log(result.keywords["2"].score);
-
-				$("#word1").html(result.keywords["0"].word+"= "+result.keywords["0"].score);
-				$("#word2").html(result.keywords["1"].word+"= "+result.keywords["1"].score);
-				$("#word3").html(result.keywords["2"].word+"= "+result.keywords["2"].score);	
-				$("#word4").html(result.keywords["3"].word+"= "+result.keywords["3"].score);
-				$("#word5").html(result.keywords["4"].word+"= "+result.keywords["4"].score);	
-            },
-            error: function (err) {
-                alert(err);
-            },
-            beforeSend: function (xhr) {
-                xhr.setRequestHeader("X-Mashape-Authorization", "UZcvZdpyc2mshN5Bh0clQLUDQ5PFp1IMTstjsn24rmcVxEWTqG"); // Enter here your Mashape key
-            }
-        });
-
-    });
 
 
